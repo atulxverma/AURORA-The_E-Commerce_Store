5a8c1e49a61d5143e85e620bac0268071f3adbb7
Atul Verma <atulv9926@gmail.com>
2025-12-19 01:22:26 +0530

    fix auth file

--- src/services/seed.ts
+++ src/services/seed.ts
@@ -1,32 +1,35 @@
 import prismaClient from "./prisma";
 
 export async function seedDB() {
-  try{
+  try {
     const res = await fetch("https://dummyjson.com/products/?limit=194");
-  const data = await res.json();
-  const Dummyproduct = data.products;
+    const data = await res.json();
+    const Dummyproduct = data.products;
 
-  //@ts-ignore
-  const filteredProducts = Dummyproduct.map((p) => {
-    return {
-      title: p.title,
-      description: p.description,
-      brand: p.brand || null,
-      category: p.category,
-      price: p.price,
-      originalPrice: p.price / (1 - p.discountPercentage / 100),
-      discountPercentage: p.discountPercentage,
-      rating: p.rating,
-      availabilityStatus: p.stock > 0 ? "In Stock" : "Out of Stock",
-      tags: p.tags || [],
-      thumbnail: p.thumbnail,
-    };
-  });
+    //@ts-ignore
+    const filteredProducts = Dummyproduct.map((p) => {
+      return {
+        title: p.title,
+        description: p.description,
+        brand: p.brand || null,
+        category: p.category,
+        price: p.price,
+        originalPrice: p.price / (1 - p.discountPercentage / 100),
+        discountPercentage: p.discountPercentage,
+        rating: p.rating,
+        availabilityStatus: p.stock > 0 ? "In Stock" : "Out of Stock",
+        tags: p.tags || [],
+        thumbnail: p.thumbnail,
+      };
+    });
 
-  await prismaClient.products.createMany({data: filteredProducts});
-  console.log("Database seeded successfully with Dummy JSON data.");
-  }
-  catch (error) {
+    // ❗ FIXED: Prisma uses "product" not "products"
+    await prismaClient.product.createMany({
+      data: filteredProducts,
+    });
+
+    console.log("Database seeded successfully with Dummy JSON data.");
+  } catch (error) {
     console.error("Error seeding database:", error);
   }
 }
